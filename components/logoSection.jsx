832c9a4e661bb87dffe1511ab5e69cb7a013b93e
hamzahope <hope@luciaprotocol.com>
2024-11-05 15:13:28 -0600

    fixed visual bugs in logosection

--- components/logoSection.jsx
+++ components/logoSection.jsx
@@ -2,14 +2,12 @@ import Marquee from 'react-fast-marquee';
 
 const LogoSection = () => {
     return (
-      <Marquee speed={50} gradient={false} className='pb-10'>
-        <div className='flex space-x-20'>
-          <img src="/images/coinsub.svg" alt="Logo 1" />
-          <img src="/images/cube3.svg" alt="Logo 2"  />
-          <img src="/images/fractal-id.svg" alt="Logo 3" />
-          <img src="/images/movement_labs.svg" alt="Logo 1" />
+      <Marquee speed={50} gradient={false} className='pb-20' autoFill={true}>
+        <img src="/images/coinsub.svg" alt="Logo 1" className='mr-40'/>
+        <img src="/images/cube3.svg" alt="Logo 2" className='mr-40' />
+        <img src="/images/fractal-id.svg" alt="Logo 3" className='mr-40' />
+        <img src="/images/movement_labs.svg" alt="Logo 1" className='mr-40'/>
           {/* Add more logos as needed */}
-        </div>
       </Marquee>
     );
   };
