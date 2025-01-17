e45a5a7096577316ddab1078211e25e89c33a9a2
DevT999 <devplay910@gmail.com>
2025-01-17 09:22:32 -0500

    add bots like real data & remove useless echo and env

--- test/bot-test.ts
+++ test/bot-test.ts
@@ -8,30 +8,106 @@ const runBotTests = async () => {
   const redLight: string = "\x1b[31m✘\x1b[0m"; // Red cross
   let allTestsPassed: boolean = true;
   const token = process.env.TOKEN;
-  const bots = 5;
+  const bots = [
+    { 
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+      viewport: { width: 1710, height: 1107 },
+      deviceScaleFactor: 2,
+      isMobile: false,
+      locale: 'en-US',
+      timezoneId: 'America/New_York',
+      plugins: [
+        { name: 'PDF Viewer' },
+        { name: 'Chrome PDF Viewer' },
+        { name: 'Chromium PDF Viewer' },
+        { name: 'Microsoft Edge PDF Viewer' },
+        { name: 'WebKit built-in PDF' },
+      ],
+      hardwareConcurrency: 8,
+      deviceMemory: 8,
+    }, { 
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15",
+      viewport: { width: 1710, height: 1107 },
+      deviceScaleFactor: 2,
+      isMobile: false,
+      locale: 'en-US',
+      timezoneId: 'America/New_York',
+      plugins: [
+        { name: 'PDF Viewer' },
+        { name: 'Chrome PDF Viewer' },
+        { name: 'Chromium PDF Viewer' },
+        { name: 'Microsoft Edge PDF Viewer' },
+        { name: 'WebKit built-in PDF' },
+      ],
+      hardwareConcurrency: 8,
+      deviceMemory: 8,
+    }, { 
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0",
+      viewport: { width: 1710, height: 1107 },
+      deviceScaleFactor: 2,
+      isMobile: false,
+      locale: 'en-US',
+      timezoneId: 'America/New_York',
+      plugins: [
+        { name: 'PDF Viewer' },
+        { name: 'Chrome PDF Viewer' },
+        { name: 'Chromium PDF Viewer' },
+        { name: 'Microsoft Edge PDF Viewer' },
+        { name: 'WebKit built-in PDF' },
+      ],
+      hardwareConcurrency: 8,
+      deviceMemory: 8,
+    }, { 
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:133.0) Gecko/20100101 Firefox/133.0",
+      viewport: { width: 1710, height: 1107 },
+      deviceScaleFactor: 2,
+      isMobile: false,
+      locale: 'en-US',
+      timezoneId: 'America/New_York',
+      plugins: [
+        { name: 'PDF Viewer' },
+        { name: 'Chrome PDF Viewer' },
+        { name: 'Chromium PDF Viewer' },
+        { name: 'Microsoft Edge PDF Viewer' },
+        { name: 'WebKit built-in PDF' },
+      ],
+      hardwareConcurrency: 8,
+      deviceMemory: 8,
+    }, { 
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Cypress/13.17.0 Chrome/118.0.5993.159 Electron/27.3.10 Safari/537.36",
+      viewport: { width: 1710, height: 1107 },
+      deviceScaleFactor: 2,
+      isMobile: false,
+      locale: 'en-US',
+      timezoneId: 'America/New_York',
+      plugins: [
+        { name: 'PDF Viewer' },
+        { name: 'Chrome PDF Viewer' },
+        { name: 'Chromium PDF Viewer' },
+        { name: 'Microsoft Edge PDF Viewer' },
+        { name: 'WebKit built-in PDF' },
+      ],
+      hardwareConcurrency: 8,
+      deviceMemory: 8,
+    },
+  ];
 
   const logResult = (testName: string, passed: boolean): void => {
     console.log(`${passed ? greenLight : redLight} ${testName}`);
     if (!passed) allTestsPassed = false;
   };
-  function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
   const browser = await chromium.launch({ headless: true });
 
   try {
     console.log("Running bot tests...");
     // Step 1: Bot Testing
     const runBotTests = async (token: string): Promise<any> => {
-      for (let i = 1; i <= bots; i++) {
-        const context = await browser.newContext({
-          userAgent: `Bot-${i}`,
-        });
+      for (const bot of bots) {
+        const context = await browser.newContext(bot);
         const page = await context.newPage();
         await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
         const firstButton = page.locator('button').first();
         await firstButton.waitFor({ state: 'attached' });
-        console.log(`Bot-${i} visited.`);
       }
 
       const fingerprints = await fetch(`${apiUrl}/dashboard/fingerprints`, {
@@ -41,7 +117,6 @@ const runBotTests = async () => {
         headers: { authorization: `Bearer ${token}` },
       });
       console.log("views", views.status);
-      await browser.close();
       // return {}
       return {
         fingerprints: fingerprints.status !== 200 ? null : await fingerprints.json(),
@@ -58,12 +133,12 @@ const runBotTests = async () => {
     // if(views === null) {
     //   throw new Error("Failed to fetch views data");
     // }
-    logResult("Test: fingerprints length validation, fingerprints: " + fingerprints.length, fingerprints.length >= bots);
-    // logResult("Test: views length validation, views: " + views.length, views.length >= bots);
-    if(fingerprints.length < bots) {
+    logResult("Test: fingerprints length validation, fingerprints: " + fingerprints.length, fingerprints.length >= bots.length);
+    // logResult("Test: views length validation, views: " + views.length, views.length >= bots.length);
+    if(fingerprints.length < bots.length) {
       process.exit(1);
     }
-    // if(views.length < bots) {
+    // if(views.length < bots.length) {
     //   process.exit(1);
     // }
 
@@ -85,6 +160,8 @@ const runBotTests = async () => {
     const errorMessage = error instanceof Error ? error.message : "Unknown error";
     console.error(`${redLight} Test suite failed: ${errorMessage}`);
     process.exit(1);
+  } finally {
+    await browser.close();
   }
 };
 
