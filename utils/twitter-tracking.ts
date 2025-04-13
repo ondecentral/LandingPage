2fc9606fe1f0d3a0fbf41fc5ad5d49c05cb27152
Ling Qing Meng <lingqingmeng@Lings-MacBook-Air.local>
2025-04-12 21:41:23 -0400

    what up danger

--- utils/twitter-tracking.ts
+++ utils/twitter-tracking.ts
@@ -6,6 +6,9 @@ declare global {
 
 export const trackTwitterConversion = () => {
   if (typeof window !== 'undefined' && window.twq) {
+    console.log('Twitter conversion event firing...');
     window.twq('event', 'tw-pi3z1-pi3z1', {});
+  } else {
+    console.warn('Twitter pixel not loaded properly');
   }
 };
